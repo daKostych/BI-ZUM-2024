c93ff58d2956de80f5275ff66aadaafe7fc9208e
Kanstantsin Downar <downakan@fit.cvut.cz>
2024-03-01 15:54:20 +0100

    DFS

--- Domácí úkoly/Počítačové úlohy/Úloha 1/maze.cpp
+++ Domácí úkoly/Počítačové úlohy/Úloha 1/maze.cpp
@@ -4,16 +4,19 @@
 #include <sstream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 struct Point
 {
     int x, y;
+
+    // bool operator==(const Point &second) const { return x == second.x && y == second.y; }
 };
 
 class Maze
 {
 public:
-    Maze(std::ifstream &inputFile, const std::string &algorithm)
+    Maze(std::ifstream &inputFile)
     {
         if (!inputFile.is_open())
         {
@@ -49,52 +52,22 @@ public:
         inputFile.close();
     };
 
-    void Solve()
+    void Solve(const std::string &algorithm)
     {
-        distance = std::vector<std::vector<int>>(maze.size(), std::vector<int>(maze[0].size(), -1));
-        parent = std::vector<std::vector<Point>>(maze.size(), std::vector<Point>(maze[0].size(), {-1, -1}));
-
-        std::queue<Point> Q;
-        Q.push(start);
-        distance[start.y][start.x] = 0;
-
-        int moveX[] = {1, -1, 0, 0};
-        int moveY[] = {0, 0, 1, -1};
-
-        while (!Q.empty())
-        {
-            Point currentPoint = Q.front();
-            Q.pop();
-
-            for (int i = 0; i < 4; i++)
-            {
-                int nextX = currentPoint.x + moveX[i];
-                int nextY = currentPoint.y + moveY[i];
-
-                if (isValidMove(nextX, nextY))
-                {
-                    if (distance[nextY][nextX] == -1)
-                    {
-                        Q.push({nextX, nextY});
-                        distance[nextY][nextX] = distance[currentPoint.y][currentPoint.x] + 1;
-                        parent[nextY][nextX] = currentPoint;
-
-                        if (destination.x == nextX && destination.y == nextY)
-                        {
-                            PrintStep(step++, true);
-                            return;
-                        }
-                    }
-                }
-            }
-            PrintStep(step++, false);
-        }
+        if (algorithm == "BFS")
+            BFS();
+        else if (algorithm == "DFS")
+            DFS();
+        else if (algorithm == "RS")
+            RandomSearch();
+        else
+            std::cout << "Invalid algorithm." << std::endl;
     };
 
     void PrintStep(size_t step, bool flag)
     {
         if (flag)
-            std::cout << "The shortest path!!!" << '\n'
+            std::cout << "Final path!!!" << '\n'
                       << std::endl;
         else
             std::cout << "Step " << step << '\n'
@@ -150,6 +123,107 @@ public:
 private:
     bool isValidMove(const int &x, const int &y) { return x >= 0 && y >= 0 && maze[y][x] != 'X'; };
 
+    void BFS()
+    {
+        distance = std::vector<std::vector<int>>(maze.size(), std::vector<int>(maze[0].size(), -1));
+        parent = std::vector<std::vector<Point>>(maze.size(), std::vector<Point>(maze[0].size(), {-1, -1}));
+
+        std::queue<Point> Q;
+        Q.push(start);
+        distance[start.y][start.x] = 0;
+
+        int moveX[] = {1, -1, 0, 0};
+        int moveY[] = {0, 0, 1, -1};
+
+        while (!Q.empty())
+        {
+            Point currentPoint = Q.front();
+            Q.pop();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nextX = currentPoint.x + moveX[i];
+                int nextY = currentPoint.y + moveY[i];
+
+                if (isValidMove(nextX, nextY))
+                {
+                    if (distance[nextY][nextX] == -1)
+                    {
+                        Q.push({nextX, nextY});
+                        distance[nextY][nextX] = distance[currentPoint.y][currentPoint.x] + 1;
+                        parent[nextY][nextX] = currentPoint;
+
+                        if (destination.x == nextX && destination.y == nextY)
+                        {
+                            PrintStep(step++, true);
+                            return;
+                        }
+                        PrintStep(step++, false);
+                    }
+                }
+            }
+            // PrintStep(step++, false);
+        }
+    };
+
+    void DFS()
+    {
+        distance = std::vector<std::vector<int>>(maze.size(), std::vector<int>(maze[0].size(), -1));
+        parent = std::vector<std::vector<Point>>(maze.size(), std::vector<Point>(maze[0].size(), {-1, -1}));
+
+        std::vector<Point> open;
+        open.push_back(start);
+        distance[start.y][start.x] = 0;
+        std::vector<Point> closed;
+
+        int moveX[] = {1, -1, 0, 0};
+        int moveY[] = {0, 0, 1, -1};
+
+        while (!open.empty())
+        {
+            Point currentPoint = open.front();
+            open.erase(open.begin());
+
+            if (currentPoint.x == destination.x && currentPoint.y == destination.y)
+            {
+                PrintStep(step++, true);
+                return;
+            }
+            else
+            {
+                std::vector<Point> currentIteration;
+                for (int i = 0; i < 4; i++)
+                {
+                    int nextX = currentPoint.x + moveX[i];
+                    int nextY = currentPoint.y + moveY[i];
+                    Point nextPoint = {nextX, nextY};
+
+                    if (isValidMove(nextX, nextY) &&
+                        ((std::find_if(open.begin(), open.end(), [&](const Point &p)
+                                       { return p.x == nextX && p.y == nextY; })) == open.end()) &&
+                        ((std::find_if(closed.begin(), closed.end(), [&](const Point &p)
+                                       { return p.x == nextX && p.y == nextY; })) == closed.end()) &&
+                        ((std::find_if(currentIteration.begin(), currentIteration.end(), [&](const Point &p)
+                                       { return p.x == nextX && p.y == nextY; })) == currentIteration.end()))
+                    {
+                        currentIteration.push_back(nextPoint);
+                        // open.insert(open.begin(), nextPoint);
+                        distance[nextY][nextX] = distance[currentPoint.y][currentPoint.x] + 1;
+                        parent[nextY][nextX] = currentPoint;
+                        PrintStep(step++, false);
+                    }
+                }
+                currentIteration.insert(currentIteration.end(), open.begin(), open.end());
+                open = currentIteration;
+                closed.push_back(currentPoint);
+                currentIteration.clear();
+                // PrintStep(step++, false);
+            }
+        }
+    };
+
+    void RandomSearch(){};
+
     std::vector<std::vector<char>> maze;
     std::vector<std::vector<int>> distance;
     std::vector<std::vector<Point>> parent;
@@ -167,11 +241,14 @@ int main(int argc, char *argv[])
     }
 
     std::ifstream input(argv[1]);
-    // std::ifstream input("dataset/6.txt");
+    // std::ifstream input("dataset/4.txt");
 
-    Maze maze = Maze(input, argv[2]);
-    // Maze maze = Maze(input, "BFS");
-    maze.Solve();
+    Maze maze = Maze(input);
+    maze.Solve(argv[2]);
+
+    // Maze maze = Maze(input);
+    // maze.Solve("DFS");
+
     maze.Clean();
 
     return 0;
